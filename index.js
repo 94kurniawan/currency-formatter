02b7ffe6ef65529d6f72aea821cf5942864945d2
dila <dila@dila-pc.com>
2021-12-11 10:27:12 +0700

    package ready to use. V.2.0.0

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function printDulu () {
-    console.log("This is a message from BK demo Package");
-  }
+function currency (number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 4,
+  }).format(number);
+}
 
-export default {printDulu}
+function separator (number) {
+  return new Intl.NumberFormat("id-ID", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 4,
+  }).format(number);
+}
+
+export {currency,separator}
